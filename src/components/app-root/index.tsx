bcd2d2c8bce5ff4c7ed3ef09b644deb6e2b3d1d0
Yota Toyama <raviqqe@gmail.com>
2022-05-29 02:54:08 +0000

    Refactor

--- src/components/app-root/index.tsx
+++ src/components/app-root/index.tsx
@@ -1,14 +1,13 @@
 import { Component, h } from "@stencil/core";
 
 @Component({ tag: "app-root", styleUrl: "index.css", shadow: true })
-export class Root {
+export class _ {
   render() {
     return (
       <div>
         <header>
           <h1>Stencil App Starter</h1>
         </header>
-
         <main>
           <stencil-router>
             <stencil-route-switch scrollTopOffset={0}>
