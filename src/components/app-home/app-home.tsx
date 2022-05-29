8194f218429553fc60dddd069abf17f7aeb4c42e
Yota Toyama <raviqqe@gmail.com>
2022-05-29 02:35:04 +0000

    Rename css files

--- src/components/app-home/app-home.tsx
+++ src/components/app-home/app-home.tsx
@@ -1,8 +1,8 @@
-import { Component, h } from '@stencil/core';
+import { Component, h } from "@stencil/core";
 
 @Component({
-  tag: 'app-home',
-  styleUrl: 'app-home.css',
+  tag: "app-home",
+  styleUrl: "style.css",
   shadow: true,
 })
 export class AppHome {
@@ -10,8 +10,9 @@ export class AppHome {
     return (
       <div class="app-home">
         <p>
-          Welcome to the Stencil App Starter. You can use this starter to build entire apps all with web components using Stencil! Check out our docs on{' '}
-          <a href="https://stenciljs.com">stenciljs.com</a> to get started.
+          Welcome to the Stencil App Starter. You can use this starter to build
+          entire apps all with web components using Stencil! Check out our docs
+          on <a href="https://stenciljs.com">stenciljs.com</a> to get started.
         </p>
 
         <stencil-route-link url="/profile/stencil">
