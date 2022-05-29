50c9dcfc1cc27bbbc2f48651c76b433e30b9f0c5
Yota Toyama <raviqqe@gmail.com>
2022-05-29 05:34:37 +0000

    Fix icon URL

--- tools/build.sh
+++ tools/build.sh
@@ -5,5 +5,5 @@ set -ex
 asset_directory=src/assets
 
 mkdir -p $asset_directory
-curl -fsSL https://pen-lang.org/favicon.svg >$asset_directory/icon.svg
+curl -fsSL https://pen-lang.s3.us-west-1.amazonaws.com/icon.svg >$asset_directory/icon.svg
 npx stencil build --prerender
