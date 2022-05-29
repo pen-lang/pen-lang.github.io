bb94eb7b64e7b627c486ccb7e444c536d4a780c9
Yota Toyama <raviqqe@gmail.com>
2022-05-29 06:01:29 +0000

    Link title

--- src/components/app-header/index.tsx
+++ src/components/app-header/index.tsx
@@ -5,7 +5,9 @@ export class _ {
   render() {
     return (
       <header>
-        <h1>Pen programming language</h1>
+        <h1>
+          <a href="/">Pen programming language</a>
+        </h1>
         <div class="links">
           <a href="https://doc.pen-lang.org">Documentation</a>
         </div>
