ca6dcc3df94854972bee4c86dec33e282aa24330
raviqqe <raviqqe@users.noreply.github.com>
2022-05-29 06:23:03 +0000

    deploy: c5f618c33589c85c52c60b61535495ead8034abc

---
+++ build/p-27e4f2b6.entry.js
@@ -0,0 +1,1 @@
+import{r,h as e}from"./p-b7cfa215.js";let o=class{constructor(e){r(this,e)}render(){return e("div",{class:"container"},e("p",null,"Hello, world!"))}};o.style=".container{padding:1rem}";export{o as app_home}
