5d4805d80ccf95be72caf2ea3d4f4937df576240
Yota Toyama <raviqqe@gmail.com>
2022-05-29 04:07:02 +0000

    Update

--- src/components/app-home/index.tsx
+++ src/components/app-home/index.tsx
@@ -4,15 +4,8 @@ import { Component, h } from "@stencil/core";
 export class _ {
   render() {
     return (
-      <div class="app-home">
-        <p>
-          Welcome to the Stencil App Starter. You can use this starter to build
-          entire apps all with web components using Stencil! Check out our docs
-          on <a href="https://stenciljs.com">stenciljs.com</a> to get started.
-        </p>
-        <stencil-route-link url="/profile/stencil">
-          <button>Profile page</button>
-        </stencil-route-link>
+      <div>
+        <p>Hello, world!</p>
       </div>
     );
   }
