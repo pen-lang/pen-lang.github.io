5c6f04ce92a9c31488f0f45619a9468b8d0859ba
Yota Toyama <raviqqe@gmail.com>
2022-05-30 04:44:12 +0000

    Update

--- src/components/app-poster/index.tsx
+++ src/components/app-poster/index.tsx
@@ -1,13 +1,40 @@
 import { Component, h } from "@stencil/core";
 
+const exampleCode = `
+import Os'Context { Context }
+import Os'File
+import Os'Process
+
+sayHello = \\(ctx Context) none | error {
+  File'Write(ctx, File'StdOut(), "Hello, world!\\n")?
+
+  none
+}
+
+main = \\(ctx context) none {
+  e = sayHello(ctx.Os)
+
+  if _ = e as none {
+    none
+  } else if error {
+    Process'Exit(ctx.Os, 1)
+  }
+}
+`;
+
 @Component({ tag: "app-poster", styleUrl: "index.css", shadow: true })
 export class _ {
   render() {
     return (
-      <div>
-        <h1>
-          Pen is the programming language for <b>scalable</b> development
-        </h1>
+      <div class="container">
+        <div>
+          <h1>
+            Pen is the programming language for <b>scalable</b> development
+          </h1>
+        </div>
+        <div>
+          <app-code>{exampleCode}</app-code>
+        </div>
       </div>
     );
   }
